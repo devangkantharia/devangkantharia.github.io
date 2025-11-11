0351e982ab718cd19cff0dad9cb68c579c9c6b51
devangkantharia <devangkantharia@gmail.com>
2025-11-11 18:08:16 +0000

    Git pages issue solved

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import createMDX from "@next/mdx";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: process.env.PAGES_BASE_PATH || '',
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   images: {
     unoptimized: true,
